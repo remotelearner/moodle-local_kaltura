d97d848dfb7c38b7698c6f89e1379b2bb1db3d95
Tyler Bannister <tyler.bannister@remote-learner.net>
2014-04-21 14:40:43 -0400

    Updated to release 3.1.02

--- version.php
+++ version.php
@@ -24,9 +24,9 @@ if (!defined('MOODLE_INTERNAL')) {
     die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
 }
 
-$plugin->version = 2013041509;
+$plugin->version = 2013041510;
 $plugin->component          = 'local_kaltura';
-$plugin->release            = 'Kaltura release 3.1.01 (RL: 2.4.1.8)';
+$plugin->release            = 'Kaltura release 3.1.02 (RL: 2.4.1.9)';
 $plugin->requires = 2012120300;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->system_version     = 2.1; // Used to send information to Kaltura
